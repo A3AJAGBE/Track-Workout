c6c48051b4da6643ab889a28d89062274ba1e610
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-21 05:16:15 +0000

    Make a post request

---
+++ main.py
@@ -0,0 +1,25 @@
+import requests
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
+
+HEADERS = {
+    "x-app-id": os.environ.get('APP_ID'),
+    "x-app-key": os.environ.get('API_KEY'),
+}
+
+exercise_info = input("What exercise(s) did you complete: ")
+
+PARAMETERS = {
+    "query": exercise_info,
+    "gender": os.environ.get('GENDER'),
+    "weight_kg": os.environ.get('WEIGHT_KG'),
+    "height_cm": os.environ.get('HEIGHT_CM'),
+    "age": os.environ.get('AGE')
+}
+
+response = requests.post(ENDPOINT, json=PARAMETERS, headers=HEADERS)
+result = response.json()
+print(result)
