010493d7de4fe7c71c74eb5d0fe149d91f3f08bb
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-21 05:55:24 +0000

    Add data to google sheet using sheety api

--- main.py
+++ main.py
@@ -4,29 +4,46 @@ from datetime import datetime
 from dotenv import load_dotenv
 load_dotenv()
 
+ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
+
+HEADERS = {
+    "x-app-id": os.environ.get('APP_ID'),
+    "x-app-key": os.environ.get('API_KEY'),
+}
+
+exercise_info = input("What exercise(s) did you complete: ")
+
+PARAMETERS = {
+    "query": exercise_info,
+    "gender": os.environ.get('GENDER'),
+    "weight_kg": os.environ.get('WEIGHT_KG'),
+    "height_cm": os.environ.get('HEIGHT_CM'),
+    "age": os.environ.get('AGE')
+}
+
+response = requests.post(ENDPOINT, json=PARAMETERS, headers=HEADERS)
+exercise_data = response.json()
+
+# Get the current date and time
 current_datetime = datetime.now()
 date = current_datetime.strftime("%x")
 time = current_datetime.strftime("%X")
-print(date, time)
 
+SHEETY_ENDPOINT = os.environ.get('SHEETY_ENDPOINT')
 
-# ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
-#
-# HEADERS = {
-#     "x-app-id": os.environ.get('APP_ID'),
-#     "x-app-key": os.environ.get('API_KEY'),
-# }
-#
-# exercise_info = input("What exercise(s) did you complete: ")
-#
-# PARAMETERS = {
-#     "query": exercise_info,
-#     "gender": os.environ.get('GENDER'),
-#     "weight_kg": os.environ.get('WEIGHT_KG'),
-#     "height_cm": os.environ.get('HEIGHT_CM'),
-#     "age": os.environ.get('AGE')
-# }
-#
-# response = requests.post(ENDPOINT, json=PARAMETERS, headers=HEADERS)
-# result = response.json()
-# print(result)
+# Add the data into google sheets using sheety api endpoint
+for exercise in exercise_data['exercises']:
+    sheety_inputs = {
+        "workout": {
+            "date": date,
+            "time": time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+    }
+
+    sheety_response = requests.post(SHEETY_ENDPOINT, json=sheety_inputs)
+
+    print(sheety_response.text)
+
